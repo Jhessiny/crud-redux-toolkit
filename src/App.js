b2590b54a3a3f6448a4d3bd97d2470ee081f6fca
Jhessiny <jhessinymattos@gmail.com>
2022-10-28 09:57:47 -0300

    test

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { useSelector } from "react-redux";
 import UserList from "./components/user-list/user-list";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const renderCount = useRef(0);
   const userList = useSelector((state) => state.users.value);
   const notify = () => toast.success("Wow so easy!");
+  useEffect(() => (renderCount.current += 1));
+  const user = {};
+
+  const methodDoesNotExist = () => {
+    user.name.oi = "oi";
+  };
 
   return (
     <Box sx={{ width: "100vw", height: "100vh", backgroundColor: "#eee" }}>
       <UserForm />
+      {renderCount.current}
       <UserList users={userList} />
       <button onClick={notify}>Notify!</button>
       <ToastContainer />
+      <button onClick={methodDoesNotExist}>Break the world</button>;
     </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/config";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://167d8b374a074163b813e4c63daf3017@o1383500.ingest.sentry.io/6700619",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
